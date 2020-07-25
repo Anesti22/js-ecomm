e913f742543f6de772ad97b6bbb5c27d25116435
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-07-25 22:39:13 +0200

    Implementing the Users Repository, Opening the Repo Data File, Saving Records

---
+++ repositories/users.js
@@ -0,0 +1,45 @@
+const { json } = require('express');
+const fs = require('fs');
+
+class UsersRepository {
+    constructor(filename) {
+        if(!filename) {
+            throw new Error('Creating a repository requires a filename');
+        }
+
+        this.filename = filename;
+
+        try {
+            fs.accessSync(this.filename);
+        } catch(err) {
+            fs.writeFileSync(this.filename, '[]')
+        }
+    }
+
+    async getAll() {
+
+        return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}));
+
+    }
+
+    async create(atters) {
+        const records = await this.getAll();
+        records.push(atters);
+
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
+}
+
+
+const test = async () => {
+
+    const repo =  new UsersRepository('users.json');
+
+    repo.create({email: 'test@test.com', password: 'password'})
+
+    const users = await repo.getAll();
+
+   console.log(users);
+}
+
+test();
